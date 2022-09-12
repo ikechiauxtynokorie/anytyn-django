c697d54a428b38ca19ae2a2bd92f05ba88881efb
Auxtyn Okorie <101457843+ikechiauxtynokorie@users.noreply.github.com>
2022-09-12 02:28:14 +0100

    payment model completed

--- orders/views.py
+++ orders/views.py
@@ -11,9 +11,9 @@ import json
 
 def payments(request):
     body = json.loads(request.body)
-    order = Order.objects.get(user = request.user, is_order = False, order_number= body['orderID'])
+    order = Order.objects.get(user = request.user, is_ordered = False, order_number= body['orderID'])
     #store transactions inside payment model
-    payment = payment(
+    payment = Payment(
         user = request.user,
         payment_id = body['tranID'],
         payment_method = body['payment_method'],
