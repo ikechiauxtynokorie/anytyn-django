bc38ce327825208e83d7728eb1ae45327b20411f
Auxtyn Okorie <101457843+ikechiauxtynokorie@users.noreply.github.com>
2022-09-18 15:05:14 +0100

    added reviews and made some model changes

--- store/views.py
+++ store/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render,get_object_or_404
-from . models import Product,Variation
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse
+from . models import Product,Variation, ReviewRating
 from cart.models import CartItem
 from category . models import Category
 from cart .views import _cart_id
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from django.db.models import Q
+from .forms import ReviewForm
+from django.contrib import messages
+
 
 
 # Create your views here.
@@ -59,3 +63,29 @@ def search(request):
         'products_count': products_count,
     }
     return render(request,'store/store.html',context)
+
+def submit_review(request,product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == "POST":
+        try:
+            review = ReviewRating.objects.get(user__id = request.user.id, product__id= product_id)
+            form = ReviewForm(request.POST, instance=reviews) 
+            form.save()
+            messages.success(request,"Thank you!. Your reviews has been updated.")
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request,"Thank you!. Your reviews has been submitted.")
+            return redirect(url)
+      
+
+        
